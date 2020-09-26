7cb5787ad651899922678bafdaa18b79fe72fe74
SaharOladi <sahar.olady@gmail.com>
2020-09-26 20:30:01 +0330

    Complete Task model.

--- app/src/main/java/com/example/mytaskmanager/model/Task.java
+++ app/src/main/java/com/example/mytaskmanager/model/Task.java
@@ -1,4 +1,74 @@
 package com.example.mytaskmanager.model;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
 public class Task {
+
+    private UUID   mTaskID;
+    private String mTaskTitle;
+    private String mTaskDescription;
+    private State  mTaskState;
+    private Date   mTaskDate;
+    private SimpleDateFormat mFormat;
+
+    public Task() {
+        mTaskID = UUID.randomUUID();
+        mTaskDate = new Date();
+    }
+
+    public UUID getTaskID() {
+        return mTaskID;
+    }
+
+    public String getTaskTitle() {
+        return mTaskTitle;
+    }
+
+    public void setTaskTitle(String taskTitle) {
+        mTaskTitle = taskTitle;
+    }
+
+    public String getTaskDescription() {
+        return mTaskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        mTaskDescription = taskDescription;
+    }
+
+    public State getTaskState() {
+        return mTaskState;
+    }
+
+    public void setTaskState(State taskState) {
+        mTaskState = taskState;
+    }
+
+    public Date getTaskDate() {
+        return mTaskDate;
+    }
+
+    public void setTaskDate(Date taskDate) {
+        mTaskDate = taskDate;
+    }
+
+    public SimpleDateFormat getFormat() {
+        return mFormat;
+    }
+
+    public void setFormat(SimpleDateFormat format) {
+        mFormat = format;
+    }
+
+    public String getJustDate(){
+        mFormat = new SimpleDateFormat("dd MMM yyyy");
+        return mFormat.format(mTaskDate);
+    }
+
+    public String getJustTime(){
+        mFormat = new SimpleDateFormat("hh:mm a");
+        return mFormat.format(mTaskDate);
+    }
 }
