f05e21addec838c8f14fdee63c830f07da58a966
SaharOladi <sahar.olady@gmail.com>
2020-09-26 21:06:11 +0330

    add task detail fragment layout.

---
+++ app/src/main/java/com/example/mytaskmanager/TaskDetailFragment.java
@@ -0,0 +1,37 @@
+package com.example.mytaskmanager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class TaskDetailFragment extends Fragment {
+
+    public TaskDetailFragment() {
+        // Required empty public constructor
+    }
+
+    public static TaskDetailFragment newInstance() {
+        TaskDetailFragment fragment = new TaskDetailFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_task_detail, container, false);
+
+        return view;
+    }
+}
