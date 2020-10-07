cc20c2513e7e03163f9a8291dd7611a813aeeb74
SaharOladi <sahar.olady@gmail.com>
2020-10-07 12:43:01 +0330

    Add database package include task database helper and schema of project ERD.

---
+++ app/src/main/java/com/example/mytaskmanager/database/TaskDBSchema.java
@@ -0,0 +1,28 @@
+package com.example.mytaskmanager.database;
+
+public class TaskDBSchema {
+
+    public static final String NAME = "task.db";
+
+    public static final class TaskTable{
+
+        public static final String NAME = "task_table";
+
+        public static final class Cols{
+
+            public static final String  ID = "id";
+            public static final String  UUID = "uuid";
+            public static final String  TITLE = "title";
+            public static final String  DESCRIPTION = "description";
+            public static final String  STATE = "state";
+            public static final String  DATE = "date";
+
+        }
+    }
+
+    public static final class UserTable{
+        //TODO
+    }
+
+
+}
